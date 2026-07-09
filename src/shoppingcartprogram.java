6a16c9caf2dc1940aaaa3ec4def58b4b623b407a
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2026-07-09 16:43:47 +0600

    Programs added

--- src/shoppingcartprogram.java
+++ src/shoppingcartprogram.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class shoppingcartprogram {
     public static void main(String[] args) {
+
+        String item;
+        double price;
+        int quantity;
+        char currency = '$';
+        double total;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What item would you like to buy: ");
+        item = scanner.nextLine();
+        System.out.print("What is the price of the item: ");
+        price = scanner.nextDouble();
+        System.out.print("How many items you take: ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+
+
+        System.out.println("\nYou have bought " + quantity + " " + item +"/s");
+        System.out.println("Your total payment is : " + currency + total);
+
+
+        scanner.close();
+
+
+
+
 
     }
 }
