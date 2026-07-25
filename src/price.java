aba1a476daadfd06e7eb07b569cf4ad377ee254a
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2026-07-26 01:25:01 +0600

    Price

--- src/price.java
+++ src/price.java
@@ -1,2 +1,57 @@
 public class price {
+    public static void main(String[] args) {
+        double price1 = 90.9999;
+        double price2 = 100.15;
+        double price3 = -580.43;
+
+        System.out.printf("%.3f\n", price1);
+        System.out.printf("%.3f\n", price2);
+        System.out.printf("%.3f\n", price3);
+
+
+        System.out.printf("%+.2f\n", price1);
+        System.out.printf("%+.2f\n", price2);
+        System.out.printf("%+.2f\n", price3);
+
+        System.out.printf("% .2f\n", price1);
+        System.out.printf("% .2f\n", price2);
+        System.out.printf("% .2f\n", price3);
+
+        System.out.printf("%,.2f\n", price1);
+        System.out.printf("%,.2f\n", price2);
+        System.out.printf("%,.2f\n", price3);
+
+        System.out.printf("%(.2f\n", price1);
+        System.out.printf("%(.2f\n", price2);
+        System.out.printf("%(.2f\n", price3);
+
+        int id1 = 1;
+        int id2 = 23;
+        int id3 = 456;
+        int id4 = 7890;
+
+        System.out.printf("%04d\n", id1);
+        System.out.printf("%04d\n", id2);
+        System.out.printf("%04d\n", id3);
+        System.out.printf("%04d\n", id4);
+
+        System.out.printf("%4d\n", id1);
+        System.out.printf("%4d\n", id2);
+        System.out.printf("%4d\n", id3);
+        System.out.printf("%4d\n", id4);
+
+        System.out.printf("%-4d\n", id1);
+        System.out.printf("%-4d\n", id2);
+        System.out.printf("%-4d\n", id3);
+        System.out.printf("%-4d\n", id4);
+
+
+
+
+
+
+
+
+
+    }
 }
