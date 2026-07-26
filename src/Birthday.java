e449eb59850b30cbf4a221b144858ec1976ff883
Shahibulfaraz096 <www.shahibul2004@gmail.com>
2026-07-26 16:25:24 +0600

    Delibery

--- src/Birthday.java
+++ src/Birthday.java
@@ -4,14 +4,41 @@ public class Birthday {
         String name = "Shahibul";
         int age = 20;
 
+        if (ageCheck(age)) {
+            System.out.println("You may sign up");
+        }
+        else {
+            System.out.println("You must be 18+ to sign up");
+        }
 
-        happybirthday(name, age);
+
+        System.out.println(square(3));
+        System.out.println(cube(3));
+
+        String fullname = getfullname("Shahibul", "Faraz");
+        System.out.println(fullname);
+
+
+
+
+
 
     }
-    static void  happybirthday(String name, int age){
-        System.out.println("Happy Birthday to you.");
-        System.out.println("Happy Birthday dear %s.");
-        System.out.println("Your are %d age now.");
+    static void  happybirthday(String name, int age) {
+        System.out.println("Happy Birthday to you");
+        System.out.printf("Happy Birthday dear %s\n", name);
+        System.out.printf("Your are %d age now\n", age);
         System.out.println("Happy Birthday to you!\n");
+    }
+    static double square (double number) {return number * number;}
+    static double cube (double number) {return number * number * number;}
+    static String getfullname (String first, String last) {return first + " " + last; }
+    static boolean ageCheck (int age) {
+        if (age >= 18) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
---
+++ src/delibery.java
@@ -0,0 +1,18 @@
+public class delibery {
+    public static void main(String[] args) {
+
+    String pizza = bakePizza("flat bread", "mozzarella", "peperoni");
+    System.out.println(pizza);
+
+    }
+    static String bakePizza(String bread) {
+        return bread + " pizza";
+    }
+    static String bakePizza(String bread, String cheese) {
+        return cheese + " " + bread + " pizza";
+    }
+    static String bakePizza(String bread, String cheese, String topping) {
+        return topping+ " " + cheese + " " + bread + " pizza";
+    }
+
+}
